0e92cdfb759bf4524e91b9444590e26520db2e18
王军伟 <wayne214@163.com>
2018-02-01 17:24:57 +0800

    完整的算是入门webpack了

--- app/Greeter.js
+++ app/Greeter.js
@@ -1,10 +1,11 @@
 // 使用ES6语法
 import React, {Component} from 'react';
 import config from './config.json';
+import styles from './Greeter.css'
 class Greeter extends Component {
     render() {
         return (
-            <div>
+            <div className={styles.root}> // 使用cssModule添加类名的方法
                 {config.greetText}
             </div>
         )
--- webpack.production.config.js
+++ webpack.production.config.js
@@ -1,19 +1,36 @@
+// webpack.production.config.js
+
 const webpack = require('webpack');
 /**
  * HtmlWebpackPlugin
- *这个插件的作用是依据一个简单的index.html模板，生成一个自动引用你打包后的JS文件的新index.html。
+ * 这个插件的作用是依据一个简单的index.html模板，生成一个自动引用你打包后的JS文件的新index.html。
  * 这在每次生成的js文件名称不同时非常有用（比如添加了hash值）。
  *
  * */
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+/**
+ * 优化插件
+ * OccurenceOrderPlugin :为组件分配ID，通过这个插件webpack可以分析和优先考虑使用最多的模块，
+ * 并为它们分配最小的ID
+ * UglifyJsPlugin：压缩JS代码；
+ * ExtractTextPlugin：分离CSS和JS文件
+ *
+ * OccurenceOrder 和 UglifyJS plugins 都是内置插件
+ * */
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+/**
+ * CleanWebpackPlugin 去除bulid文件中的残余文件
+ * */
+const CleanWebpackPlugin = require("clean-webpack-plugin");
 module.exports = {
-    devtool: 'eval-source-map',
+    // devtool: 'eval-source-map',
 
     entry: __dirname + "/app/main.js", // 唯一的入口文件
     output: {
         path: __dirname + "/build", // 打包后的文件存放的地方
-        filename: "bundle.js" // 打包后输出文件的文件名
+        filename: "bundle-[hash].js" // 打包后输出文件的文件名
     },
+    devtool: 'null',//注意修改了这里，这能大大压缩我们的打包代码
 
     devServer: {
         contentBase: "./public", //本地服务器所加载的页面所在目录
@@ -52,6 +69,14 @@ module.exports = {
         new HtmlWebpackPlugin({
             template: __dirname + "/app/index.tmpl.html" // new 一个这个插件的实例，并传入相关参数
         }),
-        new webpack.HotModuleReplacementPlugin() // 热加载插件
+        new webpack.HotModuleReplacementPlugin(), // 热加载插件
+        new webpack.optimize.OccurrenceOrderPlugin(),
+        new webpack.optimize.UglifyJsPlugin(),
+        new ExtractTextPlugin("style.css"),
+        new CleanWebpackPlugin('build/*.*', {
+            root: __dirname,
+            verbose: true,
+            dry: false
+        })
     ],
 };
